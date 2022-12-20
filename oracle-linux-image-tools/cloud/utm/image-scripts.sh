860530727791ea698cc113cb73a6186420198100
Philippe Vanhaesendonck <philippe.vanhaesendonck@oracle.com>
2022-12-20 18:46:13 +0100

    feat(olss): :sparkles: add utm cloud
    Signed-off-by: Philippe Vanhaesendonck <philippe.vanhaesendonck@oracle.com>

---
+++ oracle-linux-image-tools/cloud/utm/image-scripts.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Cleanup and package image for the "None" image
+#
+# Copyright (c) 2022 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# https://oss.oracle.com/licenses/upl
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+#
+
+#######################################
+# Image packaging: 
+#   For VistualBox we convert back to VMDK and re-create the OVA file
+#   For qemu we convert to a qcow2 file
+# Globals:
+#   CLOUD_DIR CLOUD
+#   VM_NAME
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+cloud::image_package() {
+  local utm_dir uuid
+  utm_dir="${VM_NAME}.utm"
+  mkdir -p "${utm_dir}/Images"
+  common::convert_to_qcow2 "${utm_dir}/Images/${VM_NAME}.qcow2"
+  cp "${CLOUD_DIR}/${CLOUD}/Penguin.png" "${utm_dir}"
+  uuid=$(python3 -c "import uuid; print(str(uuid.uuid4()).upper())")
+  sed \
+    -e "s/image.qcow2/${VM_NAME}.qcow2/" \
+    -e "s!opc/opc!opc/${SSH_PASSWORD}!" \
+    -e "s/00000000-0000-0000-0000-000000000000/${uuid}/" \
+    "${CLOUD_DIR}/${CLOUD}/config.plist" > "${utm_dir}/config.plist"
+  zip -r "${utm_dir}.zip" "${utm_dir}"
+  rm -rf "${utm_dir}"
+}
